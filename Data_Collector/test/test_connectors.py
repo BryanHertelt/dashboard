fdcf029886cffc6487a4e13b6aa54b148279a091
Nick  Guber <nickleonguber@gmail.com>
2022-06-07 14:14:31 +0200

    test: First test for Database_connector.

--- Data_Collector/test/test_connectors.py
+++ Data_Collector/test/test_connectors.py
@@ -4,6 +4,11 @@ import psycopg2
 from src.connectors import Database_connector
 
 def test_Database_connector():
-    pass
- 
-db = Database_connector("dbname=test user=postgres password=secret")
+    string = "dbname=crypto user=postgres password=password port=5432"
+    db = Database_connector(string)
+    print(db.connect())
+    db.cursor.excetute("CREATE TABLE test (id INTEGER, name VARCHAR(255) NOT NULL")
+    db.write("INSERT INTTO TABLE test (p1, p2) VALUES (%s, %s)", [1, "nick"])
+    db.close_connection()
+    
+test_Database_connector()
