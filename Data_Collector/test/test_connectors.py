b98d4eddda4a9faa87a67ef6de95a0899884f638
Nick  Guber <nickleonguber@gmail.com>
2022-06-10 17:40:55 +0200

    test: Created basic tests for Database_connector and Message_broker_connector.

--- Data_Collector/test/test_connectors.py
+++ Data_Collector/test/test_connectors.py
@@ -1,14 +1,13 @@
 import unittest
 import psycopg2
 
-from src.connectors import Database_connector
+from src.connectors import Database_connector, Message_broker_connector
 
 def test_Database_connector():
-    string = "dbname=crypto user=postgres password=password port=5432"
-    db = Database_connector(string)
-    print(db.connect())
-    db.cursor.excetute("CREATE TABLE test (id INTEGER, name VARCHAR(255) NOT NULL")
-    db.write("INSERT INTTO TABLE test (p1, p2) VALUES (%s, %s)", [1, "nick"])
-    db.close_connection()
-    
-test_Database_connector()
+    db = Database_connector("test")
+    db.connect()
+
+def test_Message_broker_connector():
+    mb = Message_broker_connector({"host": "localhost"})
+    mb.connect()
+    pass
