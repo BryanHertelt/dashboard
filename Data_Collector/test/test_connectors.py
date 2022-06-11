ce27a21949cd0cc29372889573a0bf8a2985be88
Nick  Guber <nickleonguber@gmail.com>
2022-06-11 21:19:57 +0200

    test: Finished test_Database_connector().

--- Data_Collector/test/test_connectors.py
+++ Data_Collector/test/test_connectors.py
@@ -4,8 +4,9 @@ import psycopg2
 from src.connectors import Database_connector, Message_broker_connector
 
 def test_Database_connector():
-    db = Database_connector("test")
+    db = Database_connector("host=localhost dbname=test user=postgres port=5433 password=root")
     db.connect()
+    db.close_connection()
 
 def test_Message_broker_connector():
     mb = Message_broker_connector({"host": "localhost"})
