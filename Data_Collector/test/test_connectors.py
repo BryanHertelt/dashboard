266cc82082286b4365c7f21f8b1e7481ca24acf4
Nick  Guber <nickleonguber@gmail.com>
2022-06-13 14:40:30 +0200

    test: Wrote psycopg2 insert test. Shows now the fastes way to insert multiple rows at once in postgresql.

--- Data_Collector/test/test_connectors.py
+++ Data_Collector/test/test_connectors.py
@@ -1,5 +1,5 @@
-import unittest
 import psycopg2
+import time
 
 from src.connectors import Database_connector, Message_broker_connector
 
@@ -12,3 +12,26 @@ def test_Message_broker_connector():
     mb = Message_broker_connector({"host": "localhost", "port": 6379})
     mb.connect()
     mb.connection.ping()
+    
+def test_db_bench():
+    TABLE_NAME = "prices"
+    CONNECTION = "postgres://postgres:password@localhost:5432/benchmark"
+    dataset = [("coin", x) for x in range(50000,100001) ]
+
+    with psycopg2.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
+        reset_query = "DROP TABLE prices;"
+        cursor.execute(reset_query)
+        conn.commit()
+        create_table = "CREATE TABLE prices (name VARCHAR(50),price INT);"
+        cursor.execute(create_table)
+        conn.commit()
+        start = time.time()
+        args_str = ",".join("('%s', '%s')" % (x, y) for (x, y) in dataset)
+        cursor.execute("INSERT INTO {table} VALUES".format(
+        table=TABLE_NAME) + args_str)
+        conn.commit()
+        
+        end = time.time()
+        
+    print(end - start)
