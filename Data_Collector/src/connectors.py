982a4456a15ac0e8b0c0ede7a5abe20e9c25ea4c
Nick  Guber <nickleonguber@gmail.com>
2022-06-21 20:30:47 +0200

    feat: Changed exception handling, renamed connection method of the websocket connector and changed close_connection().

--- Data_Collector/src/connectors.py
+++ Data_Collector/src/connectors.py
@@ -4,6 +4,7 @@ import redis
 import websockets
 import asyncio
 import psycopg2
+import json
 
 class Database_connector:
     
@@ -25,7 +26,7 @@ class Database_connector:
         
         try:
             self.connection = psycopg2.connect(self._connection_string)
-        except psycopg2.OperationalError as error:
+        except psycopg2.OperationalError:
             self._logger.exception("Connection to database failed.")
             raise
         else:
@@ -40,9 +41,10 @@ class Database_connector:
             args_str = ",".join("('%s', '%s', '%s')" % (coin, price, volume) for (coin, price, volume) in data)
             try:
                 self.cursor.execute(query_statement.format(table=table + args_str) )
-            except psycopg2.ProgrammingError as error:
+            except psycopg2.ProgrammingError:
                 self._logger.exception("Cannot execute query statement")
                 self.cursor.rollback()
+                self._loggger.warn("Rollback of query statement executed.")
                 raise
             else:
                 self.cursor.commit()
@@ -76,7 +78,7 @@ class Message_broker_connector:
                 port=self._connection_string["port"],
                 password=self._connection_string["password"],
             )
-        except redis.RedisError as error:
+        except redis.RedisError:
             self._logger.exception("Messagebroker connect failed.")
             raise
         else:
@@ -88,7 +90,7 @@ class Message_broker_connector:
         
         try:
             pipeline = self.connection.pipeline()
-        except redis.RedisError as error:
+        except redis.RedisError:
             self._logger.exception("Messagebroker write failed.")
             raise
         else:
@@ -97,7 +99,7 @@ class Message_broker_connector:
             pipeline.publish(source + item[0], item[1])
         try:
             pipeline.execute()
-        except redis.RedisError as error:
+        except redis.RedisError:
             self._logger.exception("Pipeline execute failed.")
             raise
         else:
@@ -110,56 +112,74 @@ class Websocket_connector:
     def __init__(self, websocket_url: str):
         self._websocket_url = websocket_url
         self._websocket_connection = None
+        self._logger = logging.getLogger("connectors")
+        self._logger.debug("Websocket_connector initialized.")
     
-    async def _connection_handler(self):
+    async def create_connection(self):
         
         '''This function creates a websocket connection.'''
         
         try:
             self._websocket_connection = await websockets.connect(self._websocket_url)
-        except websockets.ConnectionClose as error:
-            #Log error
+        except websockets.exceptions.ConnectionClose:
+            self._logger.exception("Websocket connection closed.")
+            #Reconnect
             raise
-        except websockets.InvalidHandshake as error:
-            #Log error
+        except websockets.exceptions.InvalidHandshake:
+            self._logger.exception("Handshake with websocket failed.")
+            #Reconnect
             raise
-        except websockets.InvalidState as error:
-            #Log error
+        except websockets.exceptions.InvalidURI:
+            self._logger.exception("Invalid websocket uri.")
             raise 
-        '''
-        async for websocket in websockets.connect(self._websocket_url):
-            try:
-                self._websocket_connection = websocket
-            except websockets.ConnectionClose as error:
-                #Log error
-                continue
-            except websockets.InvalidHandshake as error:
-                #Log error
-                raise
-            except websockets.InvalidState as error:
-                #Log error
-                raise
-        '''
     
-    async def _message_handler(self, websocket):
+    def reconnect(self):
+        pass
+    
+    
+    async def close_connection(self, reason: str ="", code: int = 1000):
+        
+        '''This function closes the websocket connection.'''
+        if self._websocket_connection:
+            await self._websocket_connection.close(reason=reason, code=code)
+            self._logger.info("Websocket connection closed.")
+            
+            
+    async def _message_handler(self, parser_func):
         
         '''This function handles every message received from the websocket.'''
         
-        async for message in websocket:
-           print(message) ###SEND DATA TO PARSER###
+        if self._websocket_connection:
+            while True:
+                try:
+                    message = await self._websocket_connection.recv()
+                except websockets.exceptions.ConnectionClose:
+                    #Reconnect
+                    self._logger.exception("Websocket connection closed.")
+                    pass
+                except websockets.exceptions.RuntimeError:
+                    self._logger.exception("Recv() method was called, from the event loop, in the same moment")
+                    raise
+                parser_func(message)
     
-    def receive_messages(self):
+    def receive_message(self):
         
         '''This function creates the connection and connect the message handler with the websocket.'''
         
-        asyncio.run(self._message_handler(self._websocket_connection))
+        asyncio.run(self._message_handler())
         
-    async def send_messages(self, message: str):
+    async def send_message(self, message: str):
+        
+        '''This function sends messages to the websocket'''
+        
         try:
-            await self._websocket_connection.send(message)
-        except websockets.ConncetionClosed as error:
-            #Log e
+            await self._websocket_connection.send(json.dumps(message))
+        except websockets.exceptions.ConncetionClosed:
+            self._logger.exception("Websocket connection closed.")
+            #Reconnect
+        except websockets.exceptions.TypeError:
+            self._logger.exception("Message type is not accepted.")
             raise
-        except websockets.TypeError as error:
-            #Log e
+        except TypeError:
+            self._logger.exception("Failed to convert message to json.")
             raise
