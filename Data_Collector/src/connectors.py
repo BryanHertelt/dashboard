0d4a8d0de914366e56266e1b1f7405e9d9afbd04
Nick  Guber <nickleonguber@gmail.com>
2022-07-05 20:32:59 +0200

    doc: Added typing for each class and docstrings for the Database_connector.

--- Data_Collector/src/connectors.py
+++ Data_Collector/src/connectors.py
@@ -1,4 +1,3 @@
-import logging
 import redis
 import websockets
 import json
@@ -7,24 +6,62 @@ import asyncio
 import aiohttp
 
 from redis import asyncio as aioredis
+from typing import Callable, Dict, List, Tuple, Union
 
 class Database_connector:
     
-    '''Object for database connections and operations.'''
+    """The connector for a PostgreSQL database.
+    
+    The Database_connector opens a connection to the database and writes into it.
+    
+    Attributes:
+        connection_string (str): 
+            A string which specifies the address to the database.
+        connection (asyncpg.Connection): 
+            A asyncpg.Connection object, which represents the connection to  the database.
+        max_reconnects (int): 
+            An integer, which is the maximum allowed number of reconnects.
+    
+    """
     
     def __init__(self, connection_string: str, max_reconnects: int) -> None: 
         
-        '''Initialize the Database_connector object.''' 
+        """Initialize the Database_connector.
         
-        self._connection_string = connection_string
-        self._cursor = None
-        self._connection = None
-        self._logger = logging.getLogger("connectors")
-        self._max_reconnects = max_reconnects
+        Args:
+            connection_string (str): 
+                A string which specifies the address to the database.
+            max_reconnects (int):
+                An integer, which is the maximum allowed number of reconnects.
+        
+        Returns:
+            None
+        
+        Raises:
+            None
+        """ 
+        
+        self._connection_string: str = connection_string
+        self._connection: asyncpg.Connection
+        self._max_reconnects: int = max_reconnects
     
-    async def connect(self) -> None: 
+    async def connect(self) -> asyncpg.Connection: 
         
-        '''This function connects to the database.'''
+        """Connects to the database.
+        
+        Connects to the database with the given address. The maximum amount of reconnects will be set. If the first connect fails, the method will retry until the amount of maximum reconnects is reached.
+        
+        Args:
+            None
+            
+        Returns:
+            The connection object will be returned in the attribute self._connection. When the connection fails, it returns nothing and instead raises an exception.
+        
+        Raises:
+            asyncpg.PostgresError, OSError:
+                This exception will be raised, if, afer the limit of reconnects is reached, there is still no working connection.
+        """
+        
         for count in range(self._max_reconnects+1):
             try:
                 self._connection = await asyncpg.connect(self._connection_string)
@@ -33,12 +70,29 @@ class Database_connector:
                 await asyncio.sleep(count)
                 continue
             else:
-                return
+                return self._connection
         raise exception
     
-    async def write(self, query_statement: str, data: list, table: str) -> None: 
+    async def write(self, query_statement: str, data: List[Tuple[str]], table: str) -> None: 
         
-        '''This function executes the query statement.'''
+        """Writes to the database.
+        
+        Writes the given data into the database. For that it uses the given query statement. Befor the insertion the data will be formatted probperly by a join()-statement. The data is inserted as its whole. To accomplish that the query is generated before and then executed.
+        
+        Args:
+            query_statement (str):
+                This string specifies the query, which will be used to insert the data into the given table.
+            data (List[Tuple[str]]):
+                The list conists of tuple containing strings. This data will be formatted and inserted entry by entry.
+            table (str):
+                This string names the table, where the data should be inserted.
+        
+        Returns:
+            None
+        
+        Raises:
+            None
+        """
         
         if self._connection != None:
             args_str = ",".join("('%s', '%s', '%s')" %(coin, price, volume) for (coin, price, volume) in data)
@@ -46,7 +100,19 @@ class Database_connector:
 
     async def close_connection(self) -> None: 
         
-        '''This function closes the connection to  the database.'''
+        """Closes the connection to the database.
+        
+        Closes the connection to the database if the connection exists.
+        
+        Args:
+            None
+            
+        Returns:
+            None
+        
+        Raises:
+            None
+        """
         
         if self._connection != None:
             await self._connection.close()
@@ -56,11 +122,11 @@ class Message_broker_connector:
     ''' Object for message broker connections and operations.'''
     
     def __init__(self, instance_url: str, max_reconnects: int) -> None:
-        self._instance_url = instance_url
-        self._connection = None
-        self._max_reconnects = max_reconnects
+        self._instance_url: str = instance_url
+        self._connection: aioredis.ConnectionPool
+        self._max_reconnects: int = max_reconnects
        
-    async def connect(self) -> None:
+    async def connect(self) -> aioredis.ConnectionPool:
         
         '''This function connects to message broker.'''
         
@@ -73,10 +139,10 @@ class Message_broker_connector:
                 await asyncio.sleep(count)
                 continue
             else:
-                return
+                return self._connection
         raise exception
     
-    async def write(self, data: list, source: str) -> None:
+    async def write(self, data: List[Tuple[str]], source: str) -> None:
         
         '''This function executes the query statement.'''
         
@@ -89,24 +155,24 @@ class Websocket_connector:
     '''Object for websocket connections and operations.'''
     
     def __init__(self, websocket_url: str, max_reconnects: int, max_size: int = None) -> None:
-        self._websocket_url = websocket_url
-        self._websocket_connection = None
-        self._max_reconnects = max_reconnects
-        self._max_size = None
+        self._websocket_url: str = websocket_url
+        self._websocket_connection: websockets.WebSocketClientProtocol
+        self._max_reconnects: int = max_reconnects
+        self._max_size: int = max_size
     
-    async def create_connection(self) -> None:
+    async def create_connection(self) -> websockets.WebSocketClientProtocol:
         
         '''This function creates a websocket connection.'''
         
         for count in range(self._max_reconnects+1):
             try:
-                self._websocket_connection = await websockets.connect(self._websocket_url)
+                self._websocket_connection = await websockets.connect(self._websocket_url, max_size=self._max_size)
             except (websockets.exceptions.ConnectionClosed, OSError, websockets.exceptions.InvalidHandshake) as error:
                 exception = error
                 await asyncio.sleep(count)
                 continue
             else:
-                return
+                return self._websocket_connection
         raise exception 
             
     async def close_connection(self, reason: str = "", code: int = 1000) -> None:
@@ -117,7 +183,7 @@ class Websocket_connector:
             await self._websocket_connection.close(reason=reason, code=code)
             self._logger.info("Websocket connection closed.")
                        
-    async def receive_message(self, parser_func) -> None:
+    async def receive_message(self, parser_func: Callable[[str], str]) -> None:
         
         '''This function handles every message received from the websocket.'''
         
@@ -134,24 +200,24 @@ class Websocket_connector:
         
 class Api_Connector:
     
-    def __init__(self, url: str, parameter: dict, timeout: int):
+    def __init__(self, url: str, parameter: Dict[str, str], timeout: int) -> None:
         
         ''' This function initializes the Api_Connector.'''
         
-        self._session = None
-        self._response = None
+        self._session: aiohttp.ClientSession
+        self._response: str
         self._url = url
-        self._status = None
-        self._params = parameter
-        self._timeout = aiohttp.ClientTimeout(total=timeout)
+        self._status: Union[str, int]
+        self._params: Dict[str, str] = parameter
+        self._timeout: aiohttp.ClientTimeout = aiohttp.ClientTimeout(total=timeout)
     
-    async def create_session(self):
+    async def create_session(self) -> None:
         
         '''This function creates a new session.'''
         
         self._session = aiohttp.ClientSession(timeout=self._timeout)
     
-    async def make_request(self):
+    async def make_request(self) -> str:
         
         '''This function creates a new request.'''
         
@@ -159,7 +225,7 @@ class Api_Connector:
             self._status = response.status
             self._response = await response.text()
     
-    async def close_session(self):
+    async def close_session(self) -> None:
         
         '''This function closes the session.'''
         
