355950b339ac496b46c911dc12aef0a53249d965
Nick  Guber <nickleonguber@gmail.com>
2022-07-07 23:17:59 +0200

    test: Created parser test file with the first tests in it.

---
+++ Data_Collector/test/test_parser.py
@@ -0,0 +1,9 @@
+def test_parser():
+    pass
+
+def test_formatting():
+    test_string = [{"data": 1, "price": 1010}, [1]]
+    result = test_string[1]["price"]
+    print(result)
+
+test_formatting()
