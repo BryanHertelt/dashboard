983d2662011aaba522d1af6ee19da81d5d366614
Nick  Guber <nickleonguber@gmail.com>
2022-07-07 20:44:36 +0200

    Removed @classmethod and created init-methods.

--- Data_Collector/src/parser.py
+++ Data_Collector/src/parser.py
@@ -1,17 +1,23 @@
 class Websocket_parser:
     
-    @classmethod
+    def __init__(self):
+        pass
+    
     def parse(self):
         pass
 
 class Api_parser:
     
-    @classmethod
+    def __init__(self):
+        pass
+    
     def parse(self):
         pass
 
 class File_parser:
     
-    @classmethod
+    def __init__(self):
+        pass
+    
     def parser(self):
         pass
