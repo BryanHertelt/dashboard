b54b42d1c1565e50b678a995f029ac344d2f8146
Nick  Guber <nickleonguber@gmail.com>
2022-07-07 20:38:44 +0200

    feat: Created first structure for the parser classes.

---
+++ Data_Collector/src/parser.py
@@ -0,0 +1,17 @@
+class Websocket_parser:
+    
+    @classmethod
+    def parse(self):
+        pass
+
+class Api_parser:
+    
+    @classmethod
+    def parse(self):
+        pass
+
+class File_parser:
+    
+    @classmethod
+    def parser(self):
+        pass
