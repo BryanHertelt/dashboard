dc812de4b1ef1620b947ab50641d27a538288367
Nick  Guber <nickleonguber@gmail.com>
2022-07-08 13:15:49 +0200

    doc: Added docstrings to the parser class.

--- Data_Collector/src/parser.py
+++ Data_Collector/src/parser.py
@@ -1,7 +1,49 @@
+from typing import Union
+
 class Parser:
     
-    def parse(self, data):
+    """Formats raw data.
+    
+    Formats raw data, which is derived from a connector. The target format is a standard format for the whole application.
+    
+    Attributes:
+        None
+    """
+    
+    def parse(self, data: Union[str,list]):
+        
+        """Parses the data.
+        
+        Parses the data with the self._formatter() method.
+        
+        Args:
+            data (Union[list, str]):
+                A list or string, which specifies the raw data.
+                
+        Returns:
+            It returns the formatted data.
+        
+        Raises:
+            None
+        """
+        
         return self._fromatter(data)
     
-    def _formatter(self, data):
+    def _formatter(self, data: Union[list, str]):
+        
+        """Formats the data.
+        
+        Formats the data and gets implemented in a subclass.
+        
+        Args:
+            data (Union[list, str]):
+                 A list or string, which specifies the raw data.
+        
+        Returns:
+            None
+            
+        Raises:
+            None   
+        """
+        
         raise NotImplementedError
