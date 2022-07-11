351d5d3e51553f110d4fd5564b9ba191e7a9855f
Nick  Guber <nickleonguber@gmail.com>
2022-07-11 17:40:23 +0200

    feat: Added some typing.

--- Data_Collector/src/collector.py
+++ Data_Collector/src/collector.py
@@ -8,7 +8,7 @@ class Collector:
         self._mb: Message_broker_connector = message_broker
         self._db: Database_connector = database
         self._parser: Parser = parser
-        self._connector = connector
+        self._connector: Union[Websocket_connector(), Api_connector()]= connector
         
     
     def _write(self):
