8d829533845ace974161ae2946e9ca9a7bfa74ac
Nick  Guber <nickleonguber@gmail.com>
2022-07-11 17:39:34 +0200

    feat: Started collector structure.

---
+++ Data_Collector/src/collector.py
@@ -0,0 +1,21 @@
+from connectors import Message_broker_connector, Database_connector, Websocket_connector, Api_connector
+from parser import Parser
+from typing import Union
+
+class Collector:
+    
+    def __init__(self, parser: Parser, message_broker: Message_broker_connector(), database: Database_connector(), connector: Union[Websocket_connector(), Api_connector()]):
+        self._mb: Message_broker_connector = message_broker
+        self._db: Database_connector = database
+        self._parser: Parser = parser
+        self._connector = connector
+        
+    
+    def _write(self):
+        pass
+    
+    def _publish(self):
+        pass
+    
+    def collect(self):
+        pass
