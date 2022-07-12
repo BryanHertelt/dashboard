357bdec3710c1b9c1ec6b3196bc77647d6da2203
Nick  Guber <nickleonguber@gmail.com>
2022-07-12 15:27:56 +0200

    feat: First non tested version of the collector.

--- Data_Collector/src/collector.py
+++ Data_Collector/src/collector.py
@@ -1,21 +1,44 @@
+import asyncio
+
 from connectors import Message_broker_connector, Database_connector, Websocket_connector, Api_connector
 from parser import Parser
 from typing import Union
 
 class Collector:
     
-    def __init__(self, parser: Parser, message_broker: Message_broker_connector(), database: Database_connector(), connector: Union[Websocket_connector(), Api_connector()]):
+    def __init__(self, parser: Parser, message_broker: Message_broker_connector, 
+                 database: Database_connector , connector: Union[Websocket_connector, Api_connector]):
         self._mb: Message_broker_connector = message_broker
         self._db: Database_connector = database
         self._parser: Parser = parser
         self._connector: Union[Websocket_connector(), Api_connector()]= connector
         
+    async def _write(self, data):
+        await self._db.connect()
+        await self._db.write(data)
     
-    def _write(self):
-        pass
+    async def _publish(self, data):
+        await self._mb.connect()
+        await self._mb.write(data)
     
-    def _publish(self):
-        pass
+    async def collect(self):
+        if type(self._connector) == Websocket_connector:
+            self._wss()
+        elif type(self._connector) == Api_connector:
+            self._http()
+        else:
+            raise ValueError("The connector has the wrong type.")
     
-    def collect(self):
-        pass
+    async def _wss(self, message: str = ""):
+        await self._connector.create_connection()
+        if message != "":
+            await self._connector.send_message(message)
+        async for message in self._connector.receive_message():
+             data = self._parser.parse(message)
+             await asyncio.gather(self._write(data), self._publish(data))
+    
+    async def _http(self):
+        await self._connector.create_session()
+        response = await self._connector.make_request()
+        data = self._parser.parse(response)
+        await asyncio.gather(self._write(data), self._publish(data))
