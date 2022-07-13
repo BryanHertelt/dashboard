710b7fc31361f999a281c33b3f883fb1e79c2bf4
Nick  Guber <nickleonguber@gmail.com>
2022-07-13 21:27:30 +0200

    feat: The Websocket_collector is now equiped with every desired functionality. There was still not testing yet.

--- Data_Collector/src/collector.py
+++ Data_Collector/src/collector.py
@@ -1,23 +1,26 @@
 import asyncio
 
 from connectors import Message_broker_connector, Database_connector, Websocket_connector, Api_connector
-from parser import Parser
+from typing import Callable
 
 class Websocket_collector:
     
-    def __init__(self, parser: Parser, message_broker: Message_broker_connector, 
+    def __init__(self, parser: Callable[[str], str], message_broker: Message_broker_connector, 
                  database: Database_connector , connector: Websocket_connector):
         self._mb: Message_broker_connector = message_broker
         self._db: Database_connector = database
-        self._parser: Parser = parser
+        self._parser: Callable[[str], str] = parser
         self._connector: Websocket_connector = connector
     
-    async def setup(self, message: str = None):
-        await self._mb.connect()
-        await self._db.connect()
-        await self._connector.create_connection()
-        if message != None:
-            await self._connector.send_message(message)
+    async def setup(self, subscription_message: str = None):
+        await asyncio.gather(
+            self._mb.connect(),
+            self._db.connect(),
+            self._connector.create_connection()
+        )
+        if subscription_message != None:
+            response = await self._connector.subscribe(subscription_message)
+            return response
        
     async def _write(self, data):
         await self._db.write(data)
@@ -30,10 +33,11 @@ class Websocket_collector:
              data = self._parser.parse(message)
              await asyncio.gather(self._write(data), self._publish(data))
 
-'''   
+
+class Api_collector:
+    
     async def _http(self):
         await self._connector.create_session()
         response = await self._connector.make_request()
         data = self._parser.parse(response)
         await asyncio.gather(self._write(data), self._publish(data))
-'''
