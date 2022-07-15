795ce1a95c1d95bbb9da5541e3091926a6eb2242
Nick  Guber <nickleonguber@gmail.com>
2022-07-15 12:28:53 +0200

    test: Created working collector tests.

---
+++ Data_Collector/test/test_collector.py
@@ -0,0 +1,36 @@
+import json
+import asyncio
+
+from src.collector import Websocket_collector, Api_collector
+from src.connectors import Websocket_connector, Api_connector, Message_broker_connector, Database_connector
+from src.parser import Parser
+
+class Wss_parser(Parser):
+    
+    @staticmethod
+    def parse(data: str):
+        parsed_data = json.loads(data)
+        return [(parsed_data[0][0], str(parsed_data[0][1]), str(parsed_data[0][2]))]
+
+async def test_Websocket_collector():
+    collector = Websocket_collector(
+        Wss_parser(), Message_broker_connector("redis://localhost:6379", 3), Database_connector("postgres://postgres:password@localhost:5432/benchmark", 3), Websocket_connector("ws://localhost:8001", 6, 2**60))
+    await collector.setup()
+    await collector.collect()
+
+#asyncio.run(test_Websocket_collector())
+
+class Api_parser(Parser):
+    
+    @staticmethod
+    def parse(data: str):
+        parsed_data = data
+        return [("BTC", "187", "187")]
+    
+async def test_Api_collector():
+    collector = Api_collector(Api_parser(), Message_broker_connector(
+        "redis://localhost:6379", 3), Database_connector("postgres://postgres:password@localhost:5432/benchmark", 3), Api_connector("https://api1.binance.com/api/v3/time", {}, 2))
+    await collector.setup()
+    await collector.collect()
+
+#asyncio.run(test_Api_collector())
