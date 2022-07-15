5b27767ee57c78558e5d02bf05452ad5f7bd7635
Nick  Guber <nickleonguber@gmail.com>
2022-07-15 12:27:00 +0200

    test: Changed test websocket so it sends more realistic data.

--- Data_Collector/test/test_endpoints.py
+++ Data_Collector/test/test_endpoints.py
@@ -1,13 +1,14 @@
 import asyncio
 import websockets
+import json
 
 
 def websocket():
   async def handler(websocket):
       while True:
-          message = await websocket.recv()
-          print(message)
-          await websocket.send("bing bong")
+          #message = await websocket.recv()
+          #print(message)
+          await websocket.send(json.dumps([("BTC", 100, 420)]))
           await asyncio.sleep(1)
 
 
