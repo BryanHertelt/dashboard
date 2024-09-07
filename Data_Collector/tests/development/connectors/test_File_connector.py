7fb57ea6352124c4fb883366b1708fd1c1df9281
Nick  Guber <nickleonguber@gmail.com>
2024-09-07 17:23:27 +0200

    struc: Cleaned the main directory.

--- Data_Collector/tests/development/connectors/test_File_connector.py
+++
@@ -1,6 +0,0 @@
-import mock 
-import pytest
-
-from src.connectors import File_connector
-
-"""No testing required, because the class just calls functions, which are already tested in their own modules."""
--- Data_Collector/tests/development/parser/test_Parser.py
+++
@@ -1,6 +0,0 @@
-import mock
-import pytest
-
-from src.parser import parser
-
-"""No testing required, because the class just acts as a parent class with not methods defined."""
