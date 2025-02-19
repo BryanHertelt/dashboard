e338680563dccbca7630dd1ea6d1222986d14e5d
Bryan Hertelt <155232447+0xbryhe@users.noreply.github.com>
2025-02-19 13:45:19 +0100

    feat: Create dummy for loading and errors while fetching.

---
+++ webfrontend/src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return <> Oh no, we couldn't find the page</>;
+}
---
+++ webfrontend/src/app/tracker/(distribution)/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSkeleton() {
+  return (
+    <>
+      <div className="card h-4/6 w-8/12 flex-grow pl-7 py-7 pr-8"></div>
+      <div className="card p-7 h-4/6 ml-7 w-3/12"></div>
+      <div className="card mt-9 p-7 w-full"></div>
+    </>
+  );
+}
