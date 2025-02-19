d0395ae126508e4ef0d2c99ec81f2231030cd27a
Bryan Hertelt <155232447+0xbryhe@users.noreply.github.com>
2025-02-19 07:55:11 +0100

    refactor: Remove types from fetch function and seperate in own file.

--- webfrontend/src/utility/lib/datafetching/layer.tsx
+++ webfrontend/src/utility/lib/datafetching/layer.tsx
@@ -1,145 +1,42 @@
-export interface PortfolioDataInterface {
-  portfolioid: number;
-  userid: number;
-  name: string;
-  currentvalue: number;
-  change7d: number[];
-  change7dpercentage: number[];
-  change3d: number[];
-  change3dpercentage: number[];
-  change24h: number[];
-  change24hpercentage: number[];
-  change12h: number[];
-  change12hpercentage: number[];
-  change4h: number[];
-  change4hpercentage: number[];
-  change1h: number[];
-  change1hpercentage: number[];
-  costbasis: number[];
-}
-export interface CryptocurrencyDataInterface {
-  symbol: string;
-  portfolioid: number;
-  userid: number;
-  groupid: number;
-  holdingid: number;
-  assettype: string;
-  assetid: number;
-  assetname: string;
-  assetabbreviation: string;
-  assetamount: number;
-  assetpercentage: number;
-  assetvalue: number;
-  assetmarketprice: number;
-  assetchange24h: number;
-  assetchange7d: number[];
-  notes: string;
-  id?: string;
-}
-export interface NFTDataInterface {
-  Symbol: string;
-  portfolioid: number;
-  userid: number;
-  groupid: number;
-  holdingid: number;
-  assettype: string;
-  assetid: number;
-  CollectionName: string;
-  CollectionValue: number;
-  CollectionFloorPrice: number;
-  NftCount: number;
-}
-export interface DerivativesDataInterface {
-  Symbol: string;
-  portfolioid: number;
-  userid: number;
-  groupid: number;
-  holdingid: number;
-  assettype: string;
-  assetid: number;
-  DerivativeName: string;
-  PositionType: string;
-  DerivativeType: string;
-  leverage: number;
-  Size: number;
-  Entry: number;
-  UnrealizedPL: number;
-  Price: number;
-  LiquidationPrice: number;
-  Margin: number;
-  TP: number;
-  SL: number;
-  SettlementDate: string | null;
-}
-export type AssetDataType =
-  | CryptocurrencyDataInterface
-  | NFTDataInterface
-  | DerivativesDataInterface;
-export interface AssetGroupDataInterface {
-  groupid: number;
-  portfolioid: number;
-  userid: number;
-  groupname: string;
-  assetcount: number;
-  groupvalue: number;
-  grouppercentage: number;
-  groupchange24h: number;
-  groupchange24hvalue: number;
-  groupchange7d: number[];
-  description: string;
-  id?: string;
-}
-export interface HoldingsDataInterface {
-  holdingid: number;
-  portfolioid: number;
-  userid: number;
-  holdingname: string;
-  assetcount: number;
-  holdingvalue: number;
-  holdingpercentage: number;
-  holdingchange24h: number;
-  holdingchange24hvalue: number;
-  holdingchange7d: number[];
-  description: string;
-  id?: string;
-}
-
+export const portfolioId = "1";
+export const baseUrl = `http://localhost:3001/${portfolioId}`;
 export const StructureLayer = {
-  fetchDistributionUnits: async function (slug: string, searchquery: string) {
+  getPortfolioData: async function () {
     const possibleSlugs = [
+      "",
       "users",
       "portfolios",
       "assets",
       "assetgroups",
       "holdings",
     ];
-    if (possibleSlugs.includes(slug)) {
-      try {
-        let rawdata = await fetch(
-          `http://localhost:4000/${slug}?${searchquery}`,
-          // while dev, need fast data updates, replace with cache strategy in build
-          { cache: "no-store" }
-        );
-        if (!rawdata.ok) {
-          throw new Error("API is not reachable");
-        }
-        let data = await rawdata.json();
-        if (
-          !Array.isArray(data) ||
-          !data.every((item) => typeof item === "object" && item !== "null")
-        ) {
-          throw new Error(
-            "Invalid response format: response isn't an array of objects"
-          );
-        }
-        return data;
-      } catch (error) {
-        console.error("Error occured in fetchDistribution Units " + `${error}`);
-        return ["failed"];
+    try {
+      let rawdata = await fetch(
+        `${baseUrl}`,
+        // while dev, need fast data updates, replace with cache strategy in build
+        { cache: "no-store" }
+      );
+      if (!rawdata.ok) {
+        throw new Error("API is not reachable");
       }
-    } else {
-      console.error(`${slug} is not a valid resource`);
-      return ["failed"];
+      let data = await rawdata.json();
+      if (
+        !Array.isArray(data.assets) ||
+        !data.assets.every(
+          (item: any) => typeof item === "object" && item !== "null"
+        )
+      ) {
+        throw new Error(
+          "Invalid response format: Assets are not an array of objects"
+        );
+      }
+      return data;
+    } catch (error) {
+      console.error("Error occured in fetchDistribution Units " + `${error}`);
+      return {
+        status: "failed",
+        errorMsg: error,
+      };
     }
   },
 };
