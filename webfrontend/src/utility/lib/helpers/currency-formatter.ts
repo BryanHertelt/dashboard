59a44918435534f5213f74dc006322248ce8f03a
Bryan Hertelt <155232447+0xbryhe@users.noreply.github.com>
2025-03-07 12:49:56 +0100

    feat: Gather helper functions in one file.

--- webfrontend/src/utility/lib/helpers/currency-formatter.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (number: number):string => {
-const formattedCurrency = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(number);
-return formattedCurrency 
-};
---
+++ webfrontend/src/utility/lib/helpers/helper-functions.tsx
@@ -0,0 +1,35 @@
+import { NftDetailImageContainer } from "./image-container";
+
+export const formatCurrency = (number: number): string => {
+  if (isNaN(Number(number))) {
+    console.error("Type error in formatCurrency");
+    return "";
+  }
+  const formattedCurrency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(Number(number));
+  return formattedCurrency;
+};
+
+export const formatValue = (number: number): string => {
+  if (isNaN(Number(number))) {
+    console.error("Type error in formatValue");
+    return "";
+  }
+  const formattedValue = Number(number).toFixed(2);
+
+  return formattedValue;
+};
+
+export const isObject = (value: any) => {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    !Array.isArray(value) &&
+    !(value instanceof RegExp) &&
+    !(value instanceof Date) &&
+    !(value instanceof Set) &&
+    !(value instanceof Map)
+  );
+};
