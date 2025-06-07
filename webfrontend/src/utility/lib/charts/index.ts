c78313b3e214b71195fe64b5e9640d836ddb7c34
Bryan Hertelt <155232447+0xbryhe@users.noreply.github.com>
2025-06-07 10:31:13 +0200

    refactor: Delete unused components.

--- webfrontend/src/utility/lib/charts/index.ts
+++ webfrontend/src/utility/lib/charts/index.ts
@@ -2,14 +2,12 @@
 //Chart builder 
 export {HoldingBarChart} from "./bar-charts"
 export {LineChartComponent}from "./line-charts"
-export {DoughnutChart}from "./doughnut-charts"
 export {TableLineChart}from "./table-line-charts"
 
 //Chart controller 
 export {default as LineChartController} from "./asset-chart-controller"
 
 //Chart Components 
-export {default as DistributionComponent} from "./distribution-component"
 export {DistributionChart} from "./distribution-chart"
 
 //Hover Labels 
